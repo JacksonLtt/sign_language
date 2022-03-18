ab2787f966721851a7ae73ba6c7c329fa5deed5f
Jackson Lu <taitingl@uci.edu>
2022-03-18 02:37:26 -0400

    up

--- Arduino/sample data/PythonNUS-master/prac.py
+++ Arduino/sample data/PythonNUS-master/prac.py
@@ -108,7 +108,7 @@ async def connect_to_device(address, loop):
             # print("Connected: {0}".format(x))
             await client.start_notify(UART_RX_UUID, callback)
             # await client.start_notify(UART_RX_UUID,  partial(my_notification_callback_with_client_input, client))
-            await asyncio.sleep(60)
+            await asyncio.sleep(40)
 
             await client.stop_notify(UART_RX_UUID)
             print("end")
@@ -120,13 +120,10 @@ async def connect_to_device(address, loop):
     print("disconnect from", address)
 
 # f = open("C:/Users/txl5518/Documents/Github/sign_language/Arduino/sample data/PythonNUS-master/7.txt",'w',newline='')
-<<<<<<< HEAD
-f = open("C:/Users/Taiting/Documents/GitHub/sign_language/Arduino/sample data/PythonNUS-master/10.txt",'w',newline='')
-# f = open("/Users/taitinglu/Documents/GitHub/sign_language/Arduino/sample data/PythonNUS-master/4.txt",'w',newline='')
-=======
 # f = open("C:/Users/Taiting/Documents/GitHub/sign_language/Arduino/sample data/PythonNUS-master/10.txt",'w',newline='')
-f = open("/Users/taitinglu/Documents/GitHub/sign_language/Arduino/sample data/PythonNUS-master/8.txt",'w',newline='')
->>>>>>> 74769db4dce994aa4514318b70fd4625f83cfaa0
+# f = open("/Users/taitinglu/Documents/GitHub/sign_language/Arduino/sample data/PythonNUS-master/4.txt",'w',newline='')
+# f = open("C:/Users/Taiting/Documents/GitHub/sign_language/Arduino/sample data/PythonNUS-master/10.txt",'w',newline='')
+f = open("/Users/taitinglu/Documents/GitHub/sign_language/Arduino/sample data/PythonNUS-master/4.txt",'w',newline='')
 
 if __name__ == "__main__":
     # addresses = [("D8:A0:1D:5D:7E:FE", "right_hand")]
@@ -139,11 +136,8 @@ if __name__ == "__main__":
     addresses = [('9C211E49-F2B3-45CE-B691-9B13D58217C9',"right hand"),('E08FC2D4-E70E-42B0-A767-07A6F555736C','left hand')]
     addresses = [("E93C311A-B0A3-41C3-800C-4B5B541FC782","Feather nRF52832")]
     addresses = [("22020732-46A7-4F91-B01D-8423CC584C1E","Feather nRF52832")]
-<<<<<<< HEAD
     addresses = [("C8:B3:A4:26:46:8F", "nRF52832 Button")]
-=======
     addresses = [("F45E7C56-20A1-487C-AA49-9137616CFF97", "nRF52832 Button")]
->>>>>>> 74769db4dce994aa4514318b70fd4625f83cfaa0
     # addresses = [('AC:67:B2:36:82:BE',"left hand")]
     # addresses = [("EF504132-F2ED-4D69-B0A5-731DCA06D098", "BatteryMonitor")]
 
